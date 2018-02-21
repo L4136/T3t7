f9525815d640ffeb35ab864cab5b4ef6cdd04f97
Heli Vepsäläinen <L4136@student.jamk.fi>
2018-02-21 21:26:15 +0200

    T3t7

---
+++ CD.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace T3t7
+{
+    class CD
+    {
+        public string Artist { get; set; }
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public double Price { get; set; }
+
+
+        public override string ToString()
+        {
+            return "Artist: " + Artist + "\nName of the album: " + Name + "\nGenre: " + Genre + "\nPrice: " + Price;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -9,6 +9,15 @@ namespace T3t7
     {
         static void Main(string[] args)
         {
+            CD cd = new CD();
+            cd.Artist = "Nightwish";
+            cd.Name = "Endless Forms Most Beautiful";
+            cd.Genre = "Symphonic metal";
+            cd.Price = 19.90;
+
+            Console.WriteLine(cd.ToString());
         }
+
+
     }
 }
